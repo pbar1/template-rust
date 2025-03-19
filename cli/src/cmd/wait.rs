397cac2906366edb92593ebca2e227e2b981dab9
Pierce Bartine <piercebartine@gmail.com>
2025-03-19 11:22:20 -0500

    feat(cli): Add timeout and select loop to wait

--- cli/src/cmd/wait.rs
+++ cli/src/cmd/wait.rs
@@ -2,6 +2,7 @@ use anyhow::Context;
 use anyhow::Result;
 use camino::Utf8PathBuf;
 use clap::Args;
+use event_pbar::Event;
 use humantime::Duration;
 use tracing::info;
 
@@ -11,7 +12,7 @@ use super::Run;
 #[derive(Debug, Args)]
 pub struct WaitArgs {
     /// Time to wait for.
-    #[clap(short, long, default_value = "3600s")]
+    #[clap(short, long, default_value = "1h")]
     duration: Duration,
 
     /// Killfile to watch.
@@ -21,19 +22,38 @@ pub struct WaitArgs {
 
 impl Run for WaitArgs {
     async fn run(&self) -> Result<()> {
-        tracing::info!("waiting forever - send a signal to exit");
+        info!(duration = %self.duration, "waiting, will timeout after duration");
 
         let killfile_path = camino::absolute_utf8(&self.killfile)
             .context("unable to get killfile absolute path")?;
+        info!(killfile = %&killfile_path, "registered watch on killfile");
 
         let event_config = event_pbar::EventConfig::builder()
             .killfile_path(killfile_path)
             .build();
 
+        let duration: tokio::time::Duration = self.duration.into();
+        let deadline = tokio::time::Instant::now() + duration;
+
         let (mut rx, _guards) = event_config.listen_tokio()?;
 
-        while let Some(event) = rx.recv().await {
-            info!(?event, "received event");
+        loop {
+            let _ = tokio::select! {
+                Some(event) = rx.recv() => {
+                    info!(?event, "received event");
+                    match event {
+                        Event::Terminate => {
+                            info!("termination event received, shutting down");
+                            break;
+                        },
+                        _other => {}
+                    };
+                }
+                _ = tokio::time::sleep_until(deadline) => {
+                    info!(duration = %self.duration, "timed out");
+                    break;
+                }
+            };
         }
 
         Ok(())
